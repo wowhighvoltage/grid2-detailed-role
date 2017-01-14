0de74b14e6434d831fe0c42bfed15f5bae61f094
High Voltage <highvoltage@deepearth.uk>
2017-01-14 20:17:25 +0000

    Code clean up round 1

--- Grid2DetailedRole/Grid2DetailedRole.lua
+++ Grid2DetailedRole/Grid2DetailedRole.lua
@@ -1,37 +1,39 @@
-local L = LibStub("AceLocale-3.0"):GetLocale("Grid2")
 local LGIST = LibStub:GetLibrary("LibGroupInSpecT-1.1")
 
 local Grid2 = Grid2
 
 local UnitClass = UnitClass
-local UnitIsEnemy= UnitIsEnemy
-local UnitIsCharmed= UnitIsCharmed
-local UnitCreatureType= UnitCreatureType
-local UnitHasVehicleUI= UnitHasVehicleUI
-
-local Color = {
-  IsActive = Grid2.statusLibrary.IsActive,
-  GetColor = Grid2.statusLibrary.GetColor,
-  GetPercent = Grid2.statusLibrary.GetPercent,
-}
+local UnitGUID = UnitGUID
+local getGroupRole = getGroupRole
+local UnitIsEnemy = UnitIsEnemy
+local UnitHasVehicleUI = UnitHasVehicleUI
 
 
-function drillDown(unit)
-    -- We've found a class whose detailed role can't be trivally determined
-    -- and we have to utilize a libraray
+function GroupInSpecTInterface(unit, default)
+    -- Use in GroupInSpecT to determine a unit's detailed role
+
     local info = LGIST:GetCachedInfo(UnitGUID(unit))
+
     if info.spec_role_detailed == nil then
         -- Can't get anymore info
-        return DAMAGER
+        return default
+    else if info.spec_role_detailed == "tank" then
+        return TANK
+    else if info.spec_role_detailed == "healer" then
+        return HEALER
     else if info.spec_role_detailed == "melee" then
         return MELEE
-    else
+    else if info.spec_role_detailed == "range" then
         return RANGE
     end
 end
 
 
 function getDetailedRole(unit)
+    -- determine a unit's detailed role. Attempts to do this using
+    -- only group role and class, but falls back to GroupInSpecT if 
+    -- this is insufficent
+
     local _, class = UnitClass(unit)
 
     if class == "MAGE" or class == "WARLOCK" then
@@ -54,7 +56,7 @@ function getDetailedRole(unit)
             return MELEE
         end
     else if class == "HUNTER" then
-        return drillDown(unit)
+        return GroupInSpecTInterface(unit, DAMAGER)
     else if class == "PRIEST" then
         if groupRole == "HEALER" then
             return HEALER
@@ -65,7 +67,7 @@ function getDetailedRole(unit)
         if getGroupRole(unit) == "HEALER" then
             return HEALER
         else
-            return drillDown(unit)
+            return GroupInSpecTInterface(unit, DAMAGER)
         end
     else if class == "DRUID" then
         local groupRole = getGroupRole(unit)
@@ -74,7 +76,7 @@ function getDetailedRole(unit)
         else if groupRole == "HEALER" then
             return HEALER
         else
-            return drillDown(unit)
+            return GroupInSpecTInterface(unit, DAMAGER)
         end
     else
         return UNKNOWN
@@ -86,50 +88,56 @@ local Lib = Grid2.statusPrototype:new("detailedrolecolor")
 
 Lib.IsActive = Grid2.statusLibrary.IsActive
 
+
 function Lib:UpdateUnit(_, unit)
-  if unit then 
-    self:UpdateIndicators(unit) 
-  end	
+    if unit then 
+        self:UpdateIndicators(unit) 
+    end	
 end
+
 
 function Lib:OnEnable()
-  self:RegisterEvent("UNIT_CLASSIFICATION_CHANGED", "UpdateUnit")
-  self:RegisterEvent("UNIT_PORTRAIT_UPDATE", "UpdateUnit")
-  self:RegisterEvent("UNIT_FLAGS", "UpdateUnit")
-  self:RegisterEvent("UNIT_FACTION", "UpdateUnit")
+    self:RegisterEvent("UNIT_CLASSIFICATION_CHANGED", "UpdateUnit")
+    self:RegisterEvent("UNIT_PORTRAIT_UPDATE", "UpdateUnit")
+    self:RegisterEvent("UNIT_FLAGS", "UpdateUnit")
+    self:RegisterEvent("UNIT_FACTION", "UpdateUnit")
 end
+
 
 function Lib:OnDisable()
-  self:UnregisterEvent("UNIT_CLASSIFICATION_CHANGED")
-  self:UnregisterEvent("UNIT_PORTRAIT_UPDATE")
-  self:UnregisterEvent("UNIT_FLAGS")
-  self:UnregisterEvent("UNIT_FACTION")
+    self:UnregisterEvent("UNIT_CLASSIFICATION_CHANGED")
+    self:UnregisterEvent("UNIT_PORTRAIT_UPDATE")
+    self:UnregisterEvent("UNIT_FLAGS")
+    self:UnregisterEvent("UNIT_FACTION")
 end
+
 
 function Lib:GetColor(unit)
-  local c = nil
-  local p = self.dbx
+    local c = nil
+    local p = self.dbx
 
-  if p.colorHostile and UnitIsCharmed(unit) and UnitIsEnemy("player", unit) then
-      c = p.colors.HOSTILE
-  else
-      c = p.colors[getDetailedRole(unit)] or p.colors.UNKNOWN_UNIT
-  end
+    if p.colorHostile and UnitIsCharmed(unit) and UnitIsEnemy("player", unit) then
+        c = p.colors.HOSTILE
+    else
+        c = p.colors[getDetailedRole(unit)] or p.colors.UNKNOWN_UNIT
+    end
 
-  return c.r, c.g, c.b, c.a
+    return c.r, c.g, c.b, c.a
 end
+
 
 Grid2.setupFunc["detailedrolecolor"] = function(baseKey, dbx)
-  Grid2:RegisterStatus(Lib, {"color"}, baseKey, dbx)
-  return Lib
+    Grid2:RegisterStatus(Lib, {"color"}, baseKey, dbx)
+    return Lib
 end
 
+
 Grid2:DbSetStatusDefaultValue( "detailedrolecolor", { type = "detailedrolecolor", colorHostile = true, colors= {
-  HOSTILE = { r = 1, g = 0.1, b = 0.1, a = 1 },
-  UNKNOWN_UNIT = { r = 0.5, g = 0.5, b = 0.5, a = 1 },
-  MELEE = { r = 0.94, g = 0.75, b = 0.28, a = 1 },
-  RANGE = { r = 0.5, g = 0.25, b = 0.69, a = 1 },
-  DAMAGER = { r = 0.5, g = 0.25, b = 0.69, a = 1 },
-  TANK = { r = 0.92, g = 0.67, b = 0.85, a = 1 },
-  HEALER = { r = 0.1, g = 0.3, b = 0.9, a = 1 }, }
+    HOSTILE = { r = 1, g = 0.1, b = 0.1, a = 1 },
+    UNKNOWN = { r = 0.5, g = 0.5, b = 0.5, a = 1 },
+    MELEE = { r = 0.94, g = 0.75, b = 0.28, a = 1 },
+    RANGE = { r = 0.5, g = 0.25, b = 0.69, a = 1 },
+    DAMAGER = { r = 0.5, g = 0.25, b = 0.69, a = 1 },
+    TANK = { r = 0.92, g = 0.67, b = 0.85, a = 1 },
+    HEALER = { r = 0.1, g = 0.3, b = 0.9, a = 1 }, }
 })
